4e134d6c2d400530b083bf15df3c7fdba9d49fd6
Matthew Downs <matthew.downsc@gmail.com>
2021-12-15 14:50:58 -0600

    Update postbuild and package.json

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,9 +1,13 @@
-const { copyFileSync, existsSync } = require('fs');
+const { copyFileSync, existsSync, writeFileSync } = require('fs');
 const { join, resolve } = require('path');
+const pkg = require('../package.json');
 
 const rootPath = resolve(__dirname, '../');
 const distPath = resolve(__dirname, '../dist');
 
-copyFileSync(join(rootPath, 'package.json'), join(distPath, 'package.json'));
+writeFileSync(join(distPath, 'package.json'), JSON.stringify({
+    ...pkg,
+    private: false
+}, null, 4));
 copyFileSync(join(rootPath, 'README.md'), join(distPath, 'README.md'));
 if (existsSync(join(rootPath, '.npmrc'))) copyFileSync(join(rootPath, '.npmrc'), join(distPath, '.npmrc'));
